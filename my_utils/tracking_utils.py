e49a33e7bb2a3ef27255809ef8dc04bc936e5bc3
Gonçalo Coutinho <goncalo.coutinho@hotmail.com>
2025-03-13 16:37:08 +0000

    Update 2 files
    - /my_utils/tracking_utils.py
    - /LICENSE.md

--- my_utils/tracking_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-def init_tracking_file(output_path, case_name):
-    os.makedirs(output_path, exist_ok=True)
-    tracking_file = output_path + '/'+ case_name + '.txt'
-    with open(tracking_file, "w") as file:
-        pass
-    file.close()
-    return tracking_file
